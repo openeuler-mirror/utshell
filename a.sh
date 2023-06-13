0a313be1634a56a7bbc6fc8cae741f11b0a93366
liutong <liutonga@uniontech.com>
2023-06-13 22:34:54 +0800

    init

---
+++ a.sh
@@ -0,0 +1,3 @@
+cd /home/tong/src/openeuler/utshell/
+git pull
+git checkout -b 4e5ff3911664c3535659c83e05c05d6d809e57d5
---
+++ x.sh
@@ -0,0 +1,46 @@
+echo cd /home/tong/src/openeuler/utshell;
+cd /home/tong/src/openeuler/utshell;
+echo git checkout -b bv2;
+git checkout -b bv2;
+cd /home/tong/src/bash;
+echo cd /home/tong/src/bash;
+git reset --hard  821d31e4f75848ca30bf5b5bede06256907b09f0   ; 
+echo git reset --hard  821d31e4f75848ca30bf5b5bede06256907b09f0   ; 
+cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
+echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
+cd /home/tong/src/openeuler/utshell
+echo cd /home/tong/src/openeuler/utshell;
+echo 2 >> record.txt
+echo echo 821d31e4f75848ca30bf5b5bede06256907b09f0  to record.txt ; 
+git add .;
+echo  git add .;
+git commit -m " init"
+echo git commit -m  init  ;
+git push  origin bv2
+echo git push  origin bv2;
+cd  /home/tong/src/bash
+echo cd  /home/tong/src/bash;
+read 
+echo cd /home/tong/src/openeuler/utshell;
+cd /home/tong/src/openeuler/utshell;
+echo git checkout -b bv3;
+git checkout -b bv3;
+cd /home/tong/src/bash;
+echo cd /home/tong/src/bash;
+git reset --hard  d8b5fe4e7673b03007c243172537bc84b7d7c558   ; 
+echo git reset --hard  d8b5fe4e7673b03007c243172537bc84b7d7c558   ; 
+cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
+echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
+cd /home/tong/src/openeuler/utshell
+echo cd /home/tong/src/openeuler/utshell;
+echo 3 >> record.txt
+echo echo d8b5fe4e7673b03007c243172537bc84b7d7c558  to record.txt ; 
+git add .;
+echo  git add .;
+git commit -m " refactor: add rust framework for builtins."
+echo git commit -m  refactor: add rust framework for builtins.  ;
+git push  origin bv3
+echo git push  origin bv3;
+cd  /home/tong/src/bash
+echo cd  /home/tong/src/bash;
+read 
