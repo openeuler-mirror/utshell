3b1b88ec2385edd4d499da09425b5aba02d38dc3
zhanghuanhuan <zhanghuanhuan@uniontech.com>
2023-06-14 13:50:01 +0800

    add README821d31e4f75848ca30bf5b5bede06256907b09f0

--- x.sh
+++ x.sh
@@ -1,230 +1,192 @@
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv4;
-git checkout -b bv4;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
+14
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv14;
+git checkout -b bv14;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
+git reset --hard  4e5ff3911664c3535659c83e05c05d6d809e57d5   ; 
+echo git reset --hard  4e5ff3911664c3535659c83e05c05d6d809e57d5   ; 
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 14 >> record.txt
+echo echo 4e5ff3911664c3535659c83e05c05d6d809e57d5  to record.txt ; 
+git add .;
+echo  git add .;
+git commit -m " add README821d31e4f75848ca30bf5b5bede06256907b09f0 "
+echo git commit -m  add README821d31e4f75848ca30bf5b5bede06256907b09f0   ;
+git push  origin bv14
+echo git push  origin bv14;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
+read 
+15
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv15;
+git checkout -b bv15;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
+git reset --hard  d8b5fe4e7673b03007c243172537bc84b7d7c558   ; 
+echo git reset --hard  d8b5fe4e7673b03007c243172537bc84b7d7c558   ; 
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 15 >> record.txt
+echo echo d8b5fe4e7673b03007c243172537bc84b7d7c558  to record.txt ; 
+git add .;
+echo  git add .;
+git commit -m " refactor: add rust framework for builtins."
+echo git commit -m  refactor: add rust framework for builtins.  ;
+git push  origin bv15
+echo git push  origin bv15;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
+read 
+16
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv16;
+git checkout -b bv16;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
+git reset --hard  9edf0cde85de2eb55837061e75b2cf882cc11e55   ; 
+echo git reset --hard  9edf0cde85de2eb55837061e75b2cf882cc11e55   ; 
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 16 >> record.txt
+echo echo 9edf0cde85de2eb55837061e75b2cf882cc11e55  to record.txt ; 
+git add .;
+echo  git add .;
+git commit -m " refactor: move builtins_org to builtins"
+echo git commit -m  refactor: move builtins_org to builtins  ;
+git push  origin bv16
+echo git push  origin bv16;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
+read 
+17
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv17;
+git checkout -b bv17;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
 git reset --hard  d337102476240db1552c114113fb8316a0b9770f   ; 
 echo git reset --hard  d337102476240db1552c114113fb8316a0b9770f   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 4 >> record.txt
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 17 >> record.txt
 echo echo d337102476240db1552c114113fb8316a0b9770f  to record.txt ; 
 git add .;
 echo  git add .;
 git commit -m " 添加jobs rust code"
 echo git commit -m  添加jobs rust code  ;
-git push  origin bv4
-echo git push  origin bv4;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
+git push  origin bv17
+echo git push  origin bv17;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
 read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv5;
-git checkout -b bv5;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
+18
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv18;
+git checkout -b bv18;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
 git reset --hard  59f05227deab020a56c870c2b7792104413ad4c7   ; 
 echo git reset --hard  59f05227deab020a56c870c2b7792104413ad4c7   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 5 >> record.txt
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 18 >> record.txt
 echo echo 59f05227deab020a56c870c2b7792104413ad4c7  to record.txt ; 
 git add .;
 echo  git add .;
 git commit -m " builtins_rust: add read plugin"
 echo git commit -m  builtins_rust: add read plugin  ;
-git push  origin bv5
-echo git push  origin bv5;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
+git push  origin bv18
+echo git push  origin bv18;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
 read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv6;
-git checkout -b bv6;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
+19
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv19;
+git checkout -b bv19;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
 git reset --hard  b33fe3ce7039944a5a18aefcc51c214a174bb25e   ; 
 echo git reset --hard  b33fe3ce7039944a5a18aefcc51c214a174bb25e   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 6 >> record.txt
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 19 >> record.txt
 echo echo b33fe3ce7039944a5a18aefcc51c214a174bb25e  to record.txt ; 
 git add .;
 echo  git add .;
 git commit -m " 优化指针传参问题"
 echo git commit -m  优化指针传参问题  ;
-git push  origin bv6
-echo git push  origin bv6;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
+git push  origin bv19
+echo git push  origin bv19;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
 read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv7;
-git checkout -b bv7;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
+20
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv20;
+git checkout -b bv20;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
 git reset --hard  7e248b9bceea2cb5a53fa8267db60951966f0d8d   ; 
 echo git reset --hard  7e248b9bceea2cb5a53fa8267db60951966f0d8d   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 7 >> record.txt
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 20 >> record.txt
 echo echo 7e248b9bceea2cb5a53fa8267db60951966f0d8d  to record.txt ; 
 git add .;
 echo  git add .;
 git commit -m " Merge "优化指针传参问题" into rustdev"
 echo git commit -m  Merge "优化指针传参问题" into rustdev  ;
-git push  origin bv7
-echo git push  origin bv7;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
+git push  origin bv20
+echo git push  origin bv20;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
 read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv8;
-git checkout -b bv8;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
+21
+echo cd /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell;
+echo git checkout -b bv21;
+git checkout -b bv21;
+cd /home/huan/gerrit/bash;
+echo cd /home/huan/gerrit/bash;
 git reset --hard  ec052778560599039fdbc50419d367acaa63b00a   ; 
 echo git reset --hard  ec052778560599039fdbc50419d367acaa63b00a   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 8 >> record.txt
+cp -r /home/huan/gerrit/bash/* /home/huan/gerrit/utshell;
+echo cp -r /home/huan/gerrit/bash/*  /home/huan/gerrit/utshell;
+cd /home/huan/gerrit/utshell
+echo cd /home/huan/gerrit/utshell;
+echo 21 >> record.txt
 echo echo ec052778560599039fdbc50419d367acaa63b00a  to record.txt ; 
 git add .;
 echo  git add .;
 git commit -m " refactor: add r_execute_cmd function"
 echo git commit -m  refactor: add r_execute_cmd function  ;
-git push  origin bv8
-echo git push  origin bv8;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
-read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv9;
-git checkout -b bv9;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
-git reset --hard  9b18ec1b6383bea95912c78856c0442e5185304c   ; 
-echo git reset --hard  9b18ec1b6383bea95912c78856c0442e5185304c   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 9 >> record.txt
-echo echo 9b18ec1b6383bea95912c78856c0442e5185304c  to record.txt ; 
-git add .;
-echo  git add .;
-git commit -m " refactor: modify execute_cmd function,part 1"
-echo git commit -m  refactor: modify execute_cmd function,part 1  ;
-git push  origin bv9
-echo git push  origin bv9;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
-read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv10;
-git checkout -b bv10;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
-git reset --hard  1efade178d50e6e98946958ab076d1e1cc3d4676   ; 
-echo git reset --hard  1efade178d50e6e98946958ab076d1e1cc3d4676   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 10 >> record.txt
-echo echo 1efade178d50e6e98946958ab076d1e1cc3d4676  to record.txt ; 
-git add .;
-echo  git add .;
-git commit -m " 添加fg_bg 命令 rust 代码"
-echo git commit -m  添加fg_bg 命令 rust 代码  ;
-git push  origin bv10
-echo git push  origin bv10;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
-read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv11;
-git checkout -b bv11;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
-git reset --hard  5a92722cbfd79d4f44b53d29c644de9449d9a9b5   ; 
-echo git reset --hard  5a92722cbfd79d4f44b53d29c644de9449d9a9b5   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 11 >> record.txt
-echo echo 5a92722cbfd79d4f44b53d29c644de9449d9a9b5  to record.txt ; 
-git add .;
-echo  git add .;
-git commit -m " Merge "refactor: modify execute_cmd function,part 1" into rustdev"
-echo git commit -m  Merge "refactor: modify execute_cmd function,part 1" into rustdev  ;
-git push  origin bv11
-echo git push  origin bv11;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
-read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv12;
-git checkout -b bv12;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
-git reset --hard  31346f4ffaae817b3242928f473619b7d06d94bd   ; 
-echo git reset --hard  31346f4ffaae817b3242928f473619b7d06d94bd   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 12 >> record.txt
-echo echo 31346f4ffaae817b3242928f473619b7d06d94bd  to record.txt ; 
-git add .;
-echo  git add .;
-git commit -m " 添加cd rust 源码,并优化指针判空写法"
-echo git commit -m  添加cd rust 源码,并优化指针判空写法  ;
-git push  origin bv12
-echo git push  origin bv12;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
-read 
-echo cd /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell;
-echo git checkout -b bv13;
-git checkout -b bv13;
-cd /home/tong/src/bash;
-echo cd /home/tong/src/bash;
-git reset --hard  8370b614793b54261b1ef08bdeb0aa36eb8f01a2   ; 
-echo git reset --hard  8370b614793b54261b1ef08bdeb0aa36eb8f01a2   ; 
-cp -r /home/tong/src/bash//* /home/tong/src/openeuler/utshell;
-echo cp -r /home/tong/src/bash//*  /home/tong/src/openeuler/utshell;
-cd /home/tong/src/openeuler/utshell
-echo cd /home/tong/src/openeuler/utshell;
-echo 13 >> record.txt
-echo echo 8370b614793b54261b1ef08bdeb0aa36eb8f01a2  to record.txt ; 
-git add .;
-echo  git add .;
-git commit -m " 1 添加fc命令源码 2 优化代码规范，去掉多余的空格 3 添加注册函数"
-echo git commit -m  1 添加fc命令源码 2 优化代码规范，去掉多余的空格 3 添加注册函数  ;
-git push  origin bv13
-echo git push  origin bv13;
-cd  /home/tong/src/bash
-echo cd  /home/tong/src/bash;
+git push  origin bv21
+echo git push  origin bv21;
+cd  /home/huan/gerrit/bash
+echo cd  /home/huan/gerrit/bash;
 read 
