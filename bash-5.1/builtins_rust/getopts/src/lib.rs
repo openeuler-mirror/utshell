fe7b870199e562598f40cc6ea536b3e496a6b9fe
liutong <liutonga@uniontech.com>
2023-06-25 16:07:56 +0800

    1 添加bash rust重构pushd 命令源码

---
+++ bash-5.1/builtins_rust/getopts/src/lib.rs
@@ -0,0 +1,558 @@
+extern crate  libc;
+extern crate nix;
+
+use libc::{c_char, c_long, c_void};
+use std::{ffi::CString};
+
+#[repr(C)]
+pub struct WORD_DESC {
+    pub word: *mut libc::c_char,
+    pub flags:libc::c_int
+}
+
+#[repr(C)]
+#[derive(Copy,Clone)]
+pub struct WORD_LIST {
+    next: *mut WORD_LIST,
+    word: *mut WORD_DESC
+}
+
+#[repr(u8)]
+enum command_type { cm_for, cm_case, cm_while, cm_if, cm_simple, cm_select,
+    cm_connection, cm_function_def, cm_until, cm_group,
+    cm_arith, cm_cond, cm_arith_for, cm_subshell, cm_coproc
+}
+
+#[repr(u8)]
+#[derive(Copy,Clone)]
+enum r_instruction {
+    r_output_direction, r_input_direction, r_inputa_direction,
+    r_appending_to, r_reading_until, r_reading_string,
+    r_duplicating_input, r_duplicating_output, r_deblank_reading_until,
+    r_close_this, r_err_and_out, r_input_output, r_output_force,
+    r_duplicating_input_word, r_duplicating_output_word,
+    r_move_input, r_move_output, r_move_input_word, r_move_output_word,
+    r_append_err_and_out
+}
+
+#[repr(C)]
+#[derive(Copy,Clone)]
+pub union REDIRECTEE {
+    dest:libc::c_int,
+    filename:* mut WORD_DESC
+}
+
+#[repr(C)]
+pub union REDIRECT {
+  next:*mut REDIRECT,
+  redirector:REDIRECTEE,
+  rflags:libc::c_int,
+  flags:libc::c_int,
+  instruction:r_instruction,
+  redirectee:REDIRECTEE,
+  here_doc_eof:*mut c_char
+}
+
+/* FOR command. */
+#[repr(C)]
+pub struct for_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    name:*mut WORD_DESC,
+    map_list:*mut WORD_LIST,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct PATTERN_LIST {
+    next:* mut PATTERN_LIST,
+    patterns:* mut WORD_LIST,
+    action:*mut COMMAND,
+    flags:libc::c_int
+}
+
+#[repr(C)]
+pub struct case_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    word:*mut WORD_DESC,
+    clauses:*mut PATTERN_LIST
+}
+
+#[repr(C)]
+pub struct while_com {
+    flags:libc::c_int,
+    test:*mut COMMAND,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct if_com {
+    flags:libc::c_int,
+    test:*mut COMMAND,
+    true_case:*mut COMMAND,
+    false_case:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct connection {
+    ignore:libc::c_int,
+    first:*mut COMMAND,
+    second:*mut COMMAND,
+    connector:libc::c_int
+}
+
+#[repr(C)]
+pub struct simple_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    words:*mut WORD_LIST,
+    redirects:*mut REDIRECT
+}
+
+#[repr(C)]
+pub struct function_def {
+    flags:libc::c_int,
+    line:libc::c_int,
+    name:*mut WORD_DESC,
+    command:*mut COMMAND,
+    source_file:*mut c_char
+}
+
+#[repr(C)]
+pub struct group_com {
+    ignore:libc::c_int,
+    command:*mut COMMAND,
+    source_file:*mut c_char
+}
+
+#[repr(C)]
+pub struct select_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    name:*mut WORD_DESC,
+    map_list:*mut WORD_LIST,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct arith_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    exp:*mut WORD_LIST
+}
+
+#[repr(C)]
+pub struct cond_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    type_c:libc::c_int,
+    exp:*mut WORD_LIST
+}
+
+#[repr(C)]
+pub struct arith_for_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    init:*mut WORD_LIST,
+    test:*mut WORD_LIST,
+    step:*mut WORD_LIST,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct subshell_com {
+    flags:i32,
+    line:i32,
+    command:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct coproc_com {
+    flags:i32,
+    name:*mut c_char,
+    command:*mut COMMAND
+}
+
+#[repr(C)]
+pub union VALUE_COMMAND {
+    For:*mut for_com,
+    Case:*mut case_com,
+    While:*mut while_com,
+    If:*mut if_com,
+    Connection:*mut connection,
+    Simple:*mut simple_com,
+    Function_def:*mut function_def,
+    Group:*mut group_com,
+    Select:*mut select_com,
+    Arith:*mut arith_com,
+    Cond:*mut cond_com,
+    ArithFor:*mut arith_for_com,
+    Subshell:*mut subshell_com,
+    Coproc:*mut coproc_com
+}
+
+#[repr(C)]
+pub struct COMMAND {
+    type_c:command_type,
+    flags:i32,
+    line:i32,
+    redirects:*mut REDIRECT,
+    value:VALUE_COMMAND
+}
+
+#[repr(C)]
+pub struct SHELL_VAR {
+  name:*mut c_char,
+  value:*mut c_char,
+  exportstr:*mut c_char,
+  dynamic_value:*mut fn(v:* mut SHELL_VAR)->*mut SHELL_VAR,
+  assign_func:* mut fn(v:* mut SHELL_VAR,str1:* mut c_char,t:c_long,str2:* mut c_char)->*mut SHELL_VAR,
+  attributes:i32,
+  context:i32
+}
+
+#[macro_export]
+macro_rules! EXECUTION_FAILURE {
+   () => {1}
+}
+
+#[macro_export]
+macro_rules! EX_USAGE {
+   () => {258}
+}
+
+#[macro_export]
+macro_rules! EXECUTION_SUCCESS {
+   () => {0}
+}
+
+#[macro_export]
+macro_rules! EX_MISCERROR {
+   () => {2}
+}
+
+#[macro_export]
+macro_rules! att_readonly {
+  () => {
+    0x0000002 /* cannot change */
+  }
+}
+
+#[macro_export]
+macro_rules! att_noassign {
+  () => {
+    0x0004000	/* assignment not allowed */
+  }
+}
+
+#[macro_export]
+macro_rules! G_EOF {
+  () => {
+    -1
+  }
+}
+
+#[macro_export]
+macro_rules! G_INVALID_OPT {
+  () => {
+    -2
+  }
+}
+
+#[macro_export]
+macro_rules! G_ARG_MISSING {
+  () => {
+    -3
+  }
+}
+
+#[macro_export]
+macro_rules! GETOPT_HELP {
+  () => {
+    -99
+  }
+}
+
+extern "C" {
+    fn unbind_variable_noref (name: * const c_char)->i32;
+    static mut sh_optind:i32;
+    static mut sh_badopt:i32;
+    fn legal_identifier (name: * const c_char)->i32;
+    fn bind_variable (name: * const c_char, value: * mut c_char, flags:i32)->* mut SHELL_VAR;
+    fn sh_invalidid(name:* mut c_char);
+    fn builtin_usage();
+    static mut sh_opterr:i32;
+    fn sh_getopt_restore_state (argv:*mut*mut c_char);
+    static dollar_vars:[* mut c_char;10];
+    fn sh_getopt (argc:i32, argv: * const*mut c_char, optstring: * const c_char)->i32;
+    static rest_of_args:* mut WORD_LIST;
+    fn number_of_args ()->i32;
+    fn strvec_create (i:i32)->*mut*mut c_char;
+    static sh_optarg:* mut c_char;
+    static sh_optopt:i32;
+    fn reset_internal_getopt();
+    fn internal_getopt (list:*mut WORD_LIST , opts:*mut c_char)->i32;
+    fn builtin_help ();
+    static mut loptend:*mut WORD_LIST;
+    fn make_builtin_argv (list:* mut WORD_LIST, ac:* mut i32)->*mut*mut c_char;
+}
+
+/* getopts_reset is magic code for when OPTIND is reset.  N is the
+   value that has just been assigned to OPTIND. */
+#[no_mangle]
+pub extern "C" fn r_getopts_reset (newind:i32){
+  unsafe {
+    sh_optind = newind;
+    sh_badopt = 0;
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn r_getopts_unbind_variable (name:* mut c_char)->i32 {
+  unsafe {
+    return unbind_variable_noref (name);
+  }
+}
+
+fn readonly_p(va:* mut SHELL_VAR)->i32
+{
+  unsafe {
+    return (*va).attributes & att_readonly!();
+  }
+}
+
+fn noassign_p(va:* mut SHELL_VAR)->i32
+{
+  unsafe {
+    return (*va).attributes & att_noassign!();
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn r_getopts_bind_variable(name:* mut c_char, value:* mut c_char)->i32
+{
+  let v:* mut SHELL_VAR;
+  unsafe {
+    if legal_identifier(name) !=0 {
+      v = bind_variable(name, value, 0);
+      if v != std::ptr::null_mut() && (readonly_p(v) != 0 || noassign_p(v) != 0) {
+        return EX_MISCERROR!();
+      }
+
+      if v != std::ptr::null_mut() {
+        return EXECUTION_SUCCESS!();
+      } else {
+        return EXECUTION_FAILURE!();
+      }
+
+    } else {
+      sh_invalidid(name);
+      return EXECUTION_FAILURE!();
+    }
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn r_dogetopts(argc:i32, argv:*mut*mut c_char)->i32
+{
+  let mut ret:i32;
+  let special_error:i32;
+  let mut old_opterr:i32=0;
+  let mut i:i32;
+  let n:i32;
+
+  let mut strval:[c_char;2]=[0;2];
+  let mut numval:[c_char;16]=[0;16];
+  let mut optstr: * mut c_char; /* list of options */
+  let name: * mut c_char;   /* variable to get flag val */
+  let t:* mut c_char;
+  unsafe {
+    let mut argcc:i32=argc;
+    let mut argvv:*mut*mut c_char=argv;
+    if argcc < 3 {
+      builtin_usage();
+      return EX_USAGE!();
+    }
+
+    /* argv[0] is "getopts". */
+    optstr = *((argvv as usize +8) as *mut*mut c_char);
+    name = *((argvv as usize +16) as *mut*mut c_char);
+    argcc -= 2;
+    argvv = (argvv as usize +16) as *mut*mut c_char;    
+    
+    if *optstr == ':' as c_char {
+      special_error = 1;
+    } else {
+      special_error = 0;
+    }
+
+    if special_error != 0 {
+      old_opterr = sh_opterr;
+      optstr=(optstr as usize + 4) as * mut c_char;
+      sh_opterr = 0; /* suppress diagnostic messages */
+    }
+
+    if argcc > 1 {
+      sh_getopt_restore_state(&mut(*argvv));
+      t = *argvv;
+      *argvv = dollar_vars[0];
+      ret = sh_getopt(argcc, argvv, optstr);
+      *argvv = t;
+    } else if rest_of_args == std::ptr::null_mut() {
+      i=0;
+      while  i < 10 && dollar_vars[i as usize] != std::ptr::null_mut() {
+        i+=1;
+      }
+
+      sh_getopt_restore_state(&mut (dollar_vars[0] as * mut c_char));
+      ret = sh_getopt(i, &dollar_vars[0], optstr);
+    } else {
+      let mut words: * mut WORD_LIST;
+      let v:*mut*mut c_char;
+
+      i = number_of_args() + 1; /* +1 for $0 */
+      v = strvec_create(i + 1);
+      i=0;
+      while i < 10 && dollar_vars[i as usize] !=std::ptr::null_mut() {
+        *((v as usize +8*i as usize) as *mut*mut c_char)  = dollar_vars[i as usize];
+        i+=1;
+      }
+
+      words = rest_of_args;
+      while  words != std::ptr::null_mut() {
+        *((v as usize +8*i as usize) as *mut*mut c_char)  = (*(*words).word).word;
+        words = (*words).next;
+        i+=1;
+      }
+
+      *((v as usize +8*i as usize) as *mut*mut c_char)  = std::ptr::null_mut();
+      sh_getopt_restore_state(&mut(*v));
+      ret = sh_getopt(i, v, optstr);
+      libc::free(v as * mut c_void);
+    }
+
+    if special_error !=0 {
+      sh_opterr = old_opterr.clone();
+    }
+
+    /* Set the OPTIND variable in any case, to handle "--" skipping.  It's
+      highly unlikely that 14 digits will be too few. */
+    if sh_optind < 10 {
+      numval[14] = sh_optind as c_char+ '0' as c_char;
+      numval[15] = '\0' as c_char;
+      i = 14;
+    } else {
+      i=15;
+      numval[15] = '\0' as c_char;
+      n = sh_optind;
+
+      i-=1;
+      numval[i as usize] = (n % 10) as c_char + '0' as c_char;
+      while n / 10 != 0 {
+        i-=1;
+        numval[i as usize] = (n % 10) as c_char + '0' as c_char;
+      }
+    }
+
+    bind_variable(CString::new ("OPTIND").unwrap().as_ptr(), & mut numval[i as usize], 0);
+
+    /* If an error occurred, decide which one it is and set the return
+      code appropriately.  In all cases, the option character in error
+      is in OPTOPT.  If an invalid option was encountered, OPTARG is
+      NULL.  If a required option argument was missing, OPTARG points
+      to a NULL string (that is, sh_optarg[0] == 0). */
+    if ret == '?' as i32 {
+      if sh_optarg == std::ptr::null_mut() {
+        ret = G_INVALID_OPT!();
+      } else if *sh_optarg == '\0' as c_char{
+        ret = G_ARG_MISSING!();
+      }
+    }
+
+    if ret == G_EOF!() {
+      r_getopts_unbind_variable(CString::new ("OPTARG").unwrap().as_ptr()as * mut c_char);
+      r_getopts_bind_variable(name, CString::new ("?").unwrap().as_ptr() as * mut c_char);
+      return EXECUTION_FAILURE!();
+    }
+
+    if ret == G_INVALID_OPT!() {
+      /* Invalid option encountered. */
+      ret = r_getopts_bind_variable(name, CString::new ("?").unwrap().as_ptr() as * mut c_char);
+
+      if special_error !=0 {
+        strval[0] = sh_optopt as c_char;
+        strval[1] = '\0' as c_char;
+        bind_variable(CString::new ("OPTARG").unwrap().as_ptr() as * mut c_char, &mut strval[0], 0);
+      } else {
+        r_getopts_unbind_variable(CString::new ("OPTARG").unwrap().as_ptr() as * mut c_char);
+      }
+
+      return ret;
+    }
+
+    if ret == G_ARG_MISSING!() {
+      /* Required argument missing. */
+      if special_error !=0 {
+        ret = r_getopts_bind_variable(name, CString::new (":").unwrap().as_ptr() as * mut c_char);
+
+        strval[0] = sh_optopt as c_char;
+        strval[1] = '\0' as c_char;
+        bind_variable(CString::new ("OPTARG").unwrap().as_ptr() as * mut c_char, &mut strval[1], 0);
+      } else {
+        ret = r_getopts_bind_variable(name, CString::new ("?").unwrap().as_ptr() as * mut c_char);
+        r_getopts_unbind_variable(CString::new ("OPTARG").unwrap().as_ptr() as * mut c_char);
+      }
+      return ret;
+    }
+
+    bind_variable(CString::new ("OPTARG").unwrap().as_ptr() as * mut c_char, sh_optarg, 0);
+
+    strval[0] = ret as c_char;
+    strval[1] = '\0' as c_char;
+    return r_getopts_bind_variable(name, &mut strval[0]);
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn r_getopts_builtin(list: * mut WORD_LIST)->i32
+{
+  unsafe {
+    let av:*mut*mut c_char;
+    let mut ac:i32=0;
+    let mut ret:i32;
+
+    if list == std::ptr::null_mut() {
+      builtin_usage();
+      return EX_USAGE!();
+    }
+
+    reset_internal_getopt();
+    ret = internal_getopt(list, CString::new ("").unwrap().as_ptr() as * mut c_char);
+    if ret != -1 {
+      if ret == GETOPT_HELP!() {
+        builtin_help();
+      } else {
+        builtin_usage();
+      }
+
+      return EX_USAGE!();
+    }
+    let llist: * mut WORD_LIST=loptend.clone();
+    av = make_builtin_argv(llist, &mut ac);
+    ret = r_dogetopts(ac, av);
+    libc::free(av as * mut c_void);
+
+    return ret;
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn cmd_name() ->*const u8 {
+   return b"getopts" as *const u8;
+}
+
+#[no_mangle]
+pub extern "C" fn run(list : *mut WORD_LIST)->i32 {
+  return r_getopts_builtin(list);
+}
---
+++ bash-5.1/builtins_rust/source/src/lib.rs
@@ -0,0 +1,416 @@
+extern crate  libc;
+extern crate nix;
+
+use libc::{c_char, c_long, c_void};
+use std::{ffi::CString};
+
+#[repr(C)]
+pub struct WORD_DESC {
+    pub word: *mut libc::c_char,
+    pub flags:libc::c_int
+}
+
+#[repr(C)]
+#[derive(Copy,Clone)]
+pub struct WORD_LIST {
+    next: *mut WORD_LIST,
+    word: *mut WORD_DESC
+}
+
+#[repr(u8)]
+enum command_type { cm_for, cm_case, cm_while, cm_if, cm_simple, cm_select,
+    cm_connection, cm_function_def, cm_until, cm_group,
+    cm_arith, cm_cond, cm_arith_for, cm_subshell, cm_coproc
+}
+
+#[repr(u8)]
+#[derive(Copy,Clone)]
+enum r_instruction {
+    r_output_direction, r_input_direction, r_inputa_direction,
+    r_appending_to, r_reading_until, r_reading_string,
+    r_duplicating_input, r_duplicating_output, r_deblank_reading_until,
+    r_close_this, r_err_and_out, r_input_output, r_output_force,
+    r_duplicating_input_word, r_duplicating_output_word,
+    r_move_input, r_move_output, r_move_input_word, r_move_output_word,
+    r_append_err_and_out
+}
+
+#[repr(C)]
+#[derive(Copy,Clone)]
+pub union REDIRECTEE {
+    dest:libc::c_int,
+    filename:* mut WORD_DESC
+}
+
+#[repr(C)]
+pub union REDIRECT {
+  next:*mut REDIRECT,
+  redirector:REDIRECTEE,
+  rflags:libc::c_int,
+  flags:libc::c_int,
+  instruction:r_instruction,
+  redirectee:REDIRECTEE,
+  here_doc_eof:*mut c_char
+}
+
+/* FOR command. */
+#[repr(C)]
+pub struct for_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    name:*mut WORD_DESC,
+    map_list:*mut WORD_LIST,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct PATTERN_LIST {
+    next:* mut PATTERN_LIST,
+    patterns:* mut WORD_LIST,
+    action:*mut COMMAND,
+    flags:libc::c_int
+}
+
+#[repr(C)]
+pub struct case_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    word:*mut WORD_DESC,
+    clauses:*mut PATTERN_LIST
+}
+
+#[repr(C)]
+pub struct while_com {
+    flags:libc::c_int,
+    test:*mut COMMAND,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct if_com {
+    flags:libc::c_int,
+    test:*mut COMMAND,
+    true_case:*mut COMMAND,
+    false_case:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct connection {
+    ignore:libc::c_int,
+    first:*mut COMMAND,
+    second:*mut COMMAND,
+    connector:libc::c_int
+}
+
+#[repr(C)]
+pub struct simple_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    words:*mut WORD_LIST,
+    redirects:*mut REDIRECT
+}
+
+#[repr(C)]
+pub struct function_def {
+    flags:libc::c_int,
+    line:libc::c_int,
+    name:*mut WORD_DESC,
+    command:*mut COMMAND,
+    source_file:*mut c_char
+}
+
+#[repr(C)]
+pub struct group_com {
+    ignore:libc::c_int,
+    command:*mut COMMAND,
+    source_file:*mut c_char
+}
+
+#[repr(C)]
+pub struct select_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    name:*mut WORD_DESC,
+    map_list:*mut WORD_LIST,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct arith_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    exp:*mut WORD_LIST
+}
+
+#[repr(C)]
+pub struct cond_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    type_c:libc::c_int,
+    exp:*mut WORD_LIST
+}
+
+#[repr(C)]
+pub struct arith_for_com {
+    flags:libc::c_int,
+    line:libc::c_int,
+    init:*mut WORD_LIST,
+    test:*mut WORD_LIST,
+    step:*mut WORD_LIST,
+    action:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct subshell_com {
+    flags:i32,
+    line:i32,
+    command:*mut COMMAND
+}
+
+#[repr(C)]
+pub struct coproc_com {
+    flags:i32,
+    name:*mut c_char,
+    command:*mut COMMAND
+}
+
+#[repr(C)]
+pub union VALUE_COMMAND {
+    For:*mut for_com,
+    Case:*mut case_com,
+    While:*mut while_com,
+    If:*mut if_com,
+    Connection:*mut connection,
+    Simple:*mut simple_com,
+    Function_def:*mut function_def,
+    Group:*mut group_com,
+    Select:*mut select_com,
+    Arith:*mut arith_com,
+    Cond:*mut cond_com,
+    ArithFor:*mut arith_for_com,
+    Subshell:*mut subshell_com,
+    Coproc:*mut coproc_com
+}
+
+#[repr(C)]
+pub struct COMMAND {
+    type_c:command_type,
+    flags:i32,
+    line:i32,
+    redirects:*mut REDIRECT,
+    value:VALUE_COMMAND
+}
+
+#[macro_export]
+macro_rules! EXECUTION_FAILURE {
+   () => {1}
+}
+
+#[macro_export]
+macro_rules! EX_USAGE {
+   () => {258}
+}
+
+#[macro_export]
+macro_rules! ARGS_SETBLTIN {
+  () => {
+    0x04
+  }
+}
+
+#[macro_export]
+macro_rules! EXITPROG {
+  () => {
+    3
+  }
+}
+pub union Functions {
+  f_xfree:unsafe extern "C" fn(str1:* mut c_void),
+  f_maybe_pop_dollar_vars: unsafe extern "C" fn(),
+  f_maybe_set_debug_trap:unsafe extern "C" fn(* mut c_char)
+}
+
+extern "C" {
+    static variable_context:i32;
+    fn dollar_vars_changed ()->i32;
+    fn dispose_saved_dollar_vars ();
+    fn pop_dollar_vars ();
+    static mut debugging_mode:i32;
+    fn pop_args ();
+    fn set_dollar_vars_unchanged ();
+    fn invalidate_cached_quoted_dollar_at ();
+    fn no_options (list:* mut WORD_LIST)->i32;
+    static mut loptend:*mut WORD_LIST;
+    fn builtin_usage();
+    fn builtin_error(err:*const c_char,...);
+    static mut restricted:i32;
+    fn sh_restricted (word:* mut c_char);
+    static mut posixly_correct:i32;
+    static mut source_searches_cwd:i32;
+    fn printable_filename (path:* mut c_char, tab:i32)->* mut c_char;
+    fn absolute_pathname (path:* const c_char)->i32;
+    static source_uses_path:i32;
+    fn find_path_file (path:* const c_char)->* mut c_char;
+    static mut interactive_shell:i32;
+    static mut executing_command_builtin:i32;
+    static mut last_command_exit_value:i32;
+    fn jump_to_top_level(level:i32);
+    fn begin_unwind_frame (str: * mut c_char);
+    fn add_unwind_protect(f:Functions,args:* mut c_char);
+    fn maybe_set_debug_trap (str: * mut c_char);
+    fn xfree(str1:* mut c_void);
+    fn push_dollar_vars ();
+    static shell_compatibility_level:i32;
+    fn init_bash_argv();
+    fn remember_args (list:* mut WORD_LIST, argc:i32);
+    fn push_args (list:* mut WORD_LIST);
+    static mut function_trace_mode:i32; 
+    fn signal_is_trapped (sig:i32)->i32;
+    fn signal_is_ignored (sig:i32)->i32;
+    static trap_list:[* mut c_char;68];
+    fn restore_default_signal (sig:i32);
+    fn source_file (name:* const c_char, sflags:i32)->i32;
+    fn run_unwind_frame (filename:* mut c_char);
+}
+
+#[no_mangle]
+pub extern "C" fn r_maybe_pop_dollar_vars ()
+{
+  unsafe {
+    if variable_context == 0 && (dollar_vars_changed () & ARGS_SETBLTIN!()) !=0 {
+      dispose_saved_dollar_vars ();
+    } else {
+      pop_dollar_vars ();
+    }
+    if debugging_mode !=0 {
+      pop_args ();	/* restore BASH_ARGC and BASH_ARGV */
+    }
+
+    set_dollar_vars_unchanged ();
+    invalidate_cached_quoted_dollar_at ();	/* just invalidate to be safe */
+  }
+}
+
+unsafe fn  savestring(x:* mut c_char)->* mut c_char
+{
+  let str1:* mut c_char=libc::malloc(1 + libc::strlen (x as * const c_char)) as * mut c_char;
+  return libc::strcpy(str1,x as * const c_char);
+}
+
+unsafe fn TRAP_STRING(s:i32)->* mut c_char {
+  if signal_is_trapped (s) !=0 && signal_is_ignored (s) == 0 {
+    return trap_list[s as usize];
+  } else {
+      return std::ptr::null_mut();
+  }
+}
+
+unsafe fn DEBUG_TRAP()->i32
+{
+  return libc::SIGRTMAX() +1;
+}
+
+#[no_mangle]
+pub extern "C" fn r_source_builtin (list:* mut WORD_LIST)->i32
+{
+  let mut result:i32;
+  let mut filename:*mut c_char;
+  let mut debug_trap:* mut c_char;
+  let x:* mut c_char;
+  unsafe {
+  if no_options (list) !=0{
+    return EX_USAGE!();
+  }
+
+  let mut  llist:* mut WORD_LIST = loptend.clone();
+
+  if list == std::ptr::null_mut() {
+    builtin_error (CString::new("filename argument required").unwrap().as_ptr());
+    builtin_usage ();
+    return EX_USAGE!();
+  }
+
+  if restricted !=0 && libc::strchr ((*(*llist).word).word, '/' as libc::c_int) != std::ptr::null_mut() {
+      sh_restricted ((*(*llist).word).word);
+      return EXECUTION_FAILURE!();
+  }
+
+  filename = std::ptr::null_mut();
+  /* XXX -- should this be absolute_pathname? */
+  if posixly_correct !=0 && libc::strchr ((*(*llist).word).word, '/' as libc::c_int) != std::ptr::null_mut() {
+    filename = savestring ((*(*llist).word).word);
+  } else if absolute_pathname ((*(*llist).word).word) !=0 {
+    filename = savestring ((*(*llist).word).word);
+  } else if source_uses_path !=0 {
+    filename = find_path_file ((*(*llist).word).word);
+  }
+
+  if filename == std::ptr::null_mut() {
+    if source_searches_cwd == 0 {
+	    x = printable_filename ((*(*llist).word).word, 0);
+      builtin_error (CString::new("%s: file not found").unwrap().as_ptr(), x);
+      if x != (*(*llist).word).word {
+        libc::free (x as * mut c_void);
+      }
+
+      if posixly_correct !=0 && interactive_shell == 0 && executing_command_builtin == 0  {
+          last_command_exit_value = EXECUTION_FAILURE!();
+          jump_to_top_level (EXITPROG!());
+      }
+      return EXECUTION_FAILURE!();
+	  } else {
+      filename = savestring ((*(*llist).word).word);
+    }
+  }
+
+  begin_unwind_frame (CString::new("source").unwrap().as_ptr() as * mut c_char);
+  let xf:Functions=Functions{f_xfree :xfree};
+  add_unwind_protect (xf, filename);
+
+  if (*list).next != std::ptr::null_mut() {
+      push_dollar_vars ();
+      let xvars:Functions=Functions{f_maybe_pop_dollar_vars:r_maybe_pop_dollar_vars};
+      add_unwind_protect (xvars, std::ptr::null_mut());
+      if debugging_mode !=0 || shell_compatibility_level <= 44 {
+        init_bash_argv ();	/* Initialize BASH_ARGV and BASH_ARGC */
+      }
+
+      remember_args ((*list).next, 1);
+      if debugging_mode !=0 {
+        push_args ((*list).next);	/* Update BASH_ARGV and BASH_ARGC */
+      }
+
+  }
+  set_dollar_vars_unchanged ();
+
+  /* Don't inherit the DEBUG trap unless function_trace_mode (overloaded)
+     is set.  XXX - should sourced files inherit the RETURN trap?  Functions
+     don't. */
+  debug_trap = TRAP_STRING (DEBUG_TRAP());
+  if debug_trap != std::ptr::null_mut() && function_trace_mode == 0  {
+      debug_trap = savestring (debug_trap);
+      let xf1:Functions=Functions{f_xfree :xfree};
+      add_unwind_protect (xf1, debug_trap);
+
+      let xfmaybe_set_debug_trap:Functions=Functions{f_maybe_set_debug_trap :maybe_set_debug_trap};
+      add_unwind_protect (xfmaybe_set_debug_trap, debug_trap);
+      restore_default_signal (DEBUG_TRAP());
+    }
+
+  result = source_file (filename, (list !=std::ptr::null_mut() && (*list).next !=std::ptr::null_mut()) as i32);
+
+  run_unwind_frame (CString::new("source").unwrap().as_ptr() as * mut c_char);
+
+  return result;
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn cmd_name() ->*const u8 {
+   return b"source" as *const u8;
+}
+
+#[no_mangle]
+pub extern "C" fn run(list : *mut WORD_LIST)->i32 {
+  return r_source_builtin(list);
+}
