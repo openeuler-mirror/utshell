3e9b8de42b7562f418f6b42c083d44dc9f1c1610
zhanghuanhuan <zhanghuanhuan@uniontech.com>
2023-07-06 15:40:54 +0800

    add const  value MB_LEN_MAX  AND_AND

--- bash-5.1/r_print_cmd/src/lib.rs
+++ bash-5.1/r_print_cmd/src/lib.rs
@@ -1,3 +1,4 @@
+
 pub const  FUNC_MULTILINE:i32 = 0x01;
 pub const  FUNC_EXTERNAL:i32 = 0x02;
 pub static mut indentation:c_int = 0;
@@ -12,3 +13,8 @@ static mut deferred_heredocs:*mut REDIRECT = 0 as *mut REDIRECT;
 static mut group_command_nesting:c_int = 0;
 static mut indirection_string:*mut c_char = 0 as *mut c_char;
 static mut indirection_stringsiz:c_int = 0;
+
+const MB_LEN_MAX:usize = 16; //在C端这是一个宏，
+const MB_CUR_MAX:usize  =  1;
+const AND_AND:i32 = 288;
+const OR_OR:i32 = 289;
