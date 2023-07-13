ec00597f6a7574a082e2211272b68ea0a564b726
liutong <liutonga@uniontech.com>
2023-07-13 20:41:08 +0800

    add r_times_builtin

--- bash-5.1/builtins_rust/times/src/lib.rs
+++ bash-5.1/builtins_rust/times/src/lib.rs
@@ -7,18 +7,16 @@ include!(concat!("intercdep.rs"));
 
 #[no_mangle]
 pub extern "C" fn r_times_builtin(list: *mut WordList) -> i32 {
-    unsafe {
-        if no_options(list) != 0 {
-            return EX_USAGE;
-        }
 
-        let curr: libc::rusage = std::mem::zeroed();
-        let kids: libc::rusage = std::mem::zeroed();
-        libc::putchar(b'\n' as c_int);
+unsafe {
 
-        libc::getrusage(libc::RUSAGE_SELF, std::mem::transmute(&curr));
-        libc::getrusage(libc::RUSAGE_CHILDREN, std::mem::transmute(&kids));
+    if no_options(list) != 0 {
+        return EX_USAGE;
+    }
 
+    let mut curr: libc::rusage = std::mem::zeroed();
+    let mut kids: libc::rusage = std::mem::zeroed();
+    libc::putchar(b'\n' as c_int);
         print_timeval(stdout, std::mem::transmute(&curr.ru_utime));
         libc::putchar(b' ' as c_int);
         print_timeval(stdout, std::mem::transmute(&curr.ru_stime));
