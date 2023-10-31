c1540a5e93f0822d0572c3c357285ed6f1fabe7d
zhanghuanhuan <zhanghuanhuan@uniontech.com>
2023-10-31 16:21:31 +0800

    add assoc_insert func   for assoc file

--- bash-5.1/r_assoc/src/lib.rs
+++ bash-5.1/r_assoc/src/lib.rs
@@ -28,4 +28,42 @@ macro_rules! ALL_ELEMENT_SUB {
     }
 }
 
+#[no_mangle]
+pub unsafe extern "C" fn assoc_dispose(mut hash: *mut HASH_TABLE) {
+    if !hash.is_null() {
+        hash_flush(hash, None);
+        hash_dispose(hash);
+    }
+}
 
+#[no_mangle]
+pub unsafe extern "C" fn assoc_flush(mut hash: *mut HASH_TABLE) {
+    hash_flush(hash, None);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn assoc_insert(
+    mut hash: *mut HASH_TABLE,
+    mut key: *mut libc::c_char,
+    mut value: *mut libc::c_char,
+) -> libc::c_int {
+    let mut b: *mut BUCKET_CONTENTS = 0 as *mut BUCKET_CONTENTS;
+    b = hash_search(key, hash, 0x2 as libc::c_int);
+    if b.is_null() {
+        return -(1 as libc::c_int);
+    }
+    if (*b).key != key {
+        libc::free(key as *mut libc::c_void);
+    }
+    if !((*b).data).is_null() {
+        libc::free((*b).data);
+    }
+    (*b).data = 0 as *mut libc::c_void;
+    (*b)
+        .data = (if !value.is_null() {
+       savestring!(value)
+    } else {
+        0 as *mut libc::c_char 
+    }) as *mut libc::c_void;
+    return 0 as libc::c_int;
+}
