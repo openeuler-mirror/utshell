97944d58b7edc33bbe0fdfc16febb69b105a14ad
zhangxingrong <zhangxingrong@uniontech.com>
2025-12-01 14:40:56 +0800

    修复运行段错误问题,修复正则表达式在判断条件中出现判断错误现象

--- utshell-0.5/rsbuiltins.h
+++
@@ -1,5 +0,0 @@
-//# This file was modified by UnionTech Software Technology Co., Ltd. in 2023/05/30 
-#include <stdio.h>
-#include "command.h"
-int r_execute_cmd();
-int r_execute_cmd2(WORD_LIST *l);
--- utshell-0.5/vendor/fluent-resmgr/tests/resources/pl/test.ftl
+++
@@ -1,2 +0,0 @@
-hello-world = Witaj Świecie
-new-message = Nowa Wiadomość
