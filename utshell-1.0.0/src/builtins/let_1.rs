0e6cc8bf2db03f2946a2736555abb5393c509148
hailianguniontech <mahailiang@uniontech.com>
2026-06-05 11:48:11 +0800

    feat(builtins): update let_1.rs

--- utshell-1.0.0/src/builtins/let_1.rs
+++ utshell-1.0.0/src/builtins/let_1.rs
@@ -1,6 +1,3 @@
-//# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
-
-//# SPDX-License-Identifier: GPL-3.0-or-later
 use super::help::builtin_help;
 use crate::builtins::common::err_translate_fn;
 use crate::expr::evalexp;
@@ -9,73 +6,76 @@ use crate::subst::string_list;
 
 #[no_mangle]
 pub fn let_builtin(mut list: *mut WordList) -> i32 {
-    unsafe {
-        let mut ret: libc::c_long = 0;
-        let expok: libc::c_int = 0;
+    let mut ret: libc::c_long = 0;
+    let expok: libc::c_int = 0;
 
-        if !list.is_null()
+    if unsafe {
+        !list.is_null()
             && !(*list).word.is_null()
             && libc::strcmp(
                 (*((*list).word)).word,
                 "--help\0".as_ptr() as *const libc::c_char,
             ) == 0
-        {
-            builtin_help();
-            return EX_USAGE;
-        }
+    } {
+        builtin_help();
+        return EX_USAGE;
+    }
 
-        if !list.is_null() && !(*list).word.is_null() && is_option((*((*list).word)).word, b'-') {
-            list = (*list).next;
-        }
+    if unsafe {
+        !list.is_null() && !(*list).word.is_null() && is_option((*((*list).word)).word, b'-')
+    } {
+        list = unsafe { (*list).next };
+    }
 
-        if list.is_null() {
-            let names = String::from("letwarn");
-            err_translate_fn(&names, std::ptr::null_mut());
-            println!();
-            return EXECUTION_FAILURE;
-        }
+    if list.is_null() {
+        let names = String::from("letwarn");
+        err_translate_fn(&names, std::ptr::null_mut());
+        println!();
+        return EXECUTION_FAILURE;
+    }
 
-        while !list.is_null() {
-            ret = evalexp(
+    while !list.is_null() {
+        ret = unsafe {
+            evalexp(
                 (*((*list).word)).word,
                 EXP_EXPANDED,
                 std::mem::transmute(&expok),
-            );
-            if expok == 0 {
-                return EXECUTION_FAILURE;
-            }
-            list = (*list).next;
-        }
-
-        return if ret == 0 {
-            EXECUTION_FAILURE
-        } else {
-            EXECUTION_SUCCESS
+            )
         };
+        if expok == 0 {
+            return EXECUTION_FAILURE;
+        }
+        list = unsafe { (*list).next };
     }
+
+    return if ret == 0 {
+        EXECUTION_FAILURE
+    } else {
+        EXECUTION_SUCCESS
+    };
 }
 
 #[no_mangle]
 pub fn exp_builtin(list: *mut WordList) -> i32 {
-    unsafe {
-        let expok: libc::c_int = 0;
+    let expok: libc::c_int = 0;
 
-        if list.is_null() {
-            let names = String::from("letwarn");
-            err_translate_fn(&names, std::ptr::null_mut());
-            println!();
-            return EXECUTION_FAILURE;
-        }
-
-        let exp = string_list(list);
-        let ret = evalexp(exp, EXP_EXPANDED, std::mem::transmute(&expok));
-        libc::free(exp as *mut c_void);
-        return if ret == 0 || expok == 0 {
-            EXECUTION_FAILURE
-        } else {
-            EXECUTION_SUCCESS
-        };
+    if list.is_null() {
+        let names = String::from("letwarn");
+        err_translate_fn(&names, std::ptr::null_mut());
+        println!();
+        return EXECUTION_FAILURE;
     }
+
+    let exp = string_list(list);
+    let ret = unsafe { evalexp(exp, EXP_EXPANDED, std::mem::transmute(&expok)) };
+    unsafe {
+        libc::free(exp as *mut c_void);
+    }
+    return if ret == 0 || expok == 0 {
+        EXECUTION_FAILURE
+    } else {
+        EXECUTION_SUCCESS
+    };
 }
 
 fn is_option(s: *mut libc::c_char, c: u8) -> bool {
