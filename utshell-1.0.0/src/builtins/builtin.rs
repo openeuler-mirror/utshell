dafaa68a33f6a56af3bb1ccebe6661363619e970
hailianguniontech <mahailiang@uniontech.com>
2026-06-05 10:41:10 +0800

    feat(builtins): update builtin.rs

--- utshell-1.0.0/src/builtins/builtin.rs
+++ utshell-1.0.0/src/builtins/builtin.rs
@@ -7,27 +7,27 @@ use crate::src_common::*;
 
 #[no_mangle]
 pub fn builtin_builtin(mut list: *mut WordList) -> i32 {
-    unsafe {
-        let mut function: Option<sh_builtin_func_t> = None;
-        let mut command: *mut libc::c_char = 0 as *mut libc::c_char;
-        if no_options(list) != 0 {
-            return 258 as libc::c_int;
-        }
-        list = loptend;
-        if list.is_null() {
-            return 0 as libc::c_int;
-        }
-        command = (*(*list).word).word;
-        function = find_shell_builtin(command);
-        if function.is_none() {
-            sh_notbuiltin(command);
-            return 1 as libc::c_int;
-        } else {
+    let function: Option<sh_builtin_func_t>;
+    let command: *mut libc::c_char;
+    if no_options(list) != 0 {
+        return 258 as libc::c_int;
+    }
+    list = unsafe { loptend };
+    if list.is_null() {
+        return 0 as libc::c_int;
+    }
+    command = unsafe { (*(*list).word).word };
+    function = find_shell_builtin(command);
+    if function.is_none() {
+        sh_notbuiltin(command);
+        return 1 as libc::c_int;
+    } else {
+        unsafe {
             this_command_name = command;
             this_shell_builtin = function;
             list = (*list).next;
-            return (Some(function.expect("non-null function pointer")))
-                .expect("non-null function pointer")(list);
-        };
-    }
+        }
+        return (Some(function.expect("non-null function pointer")))
+            .expect("non-null function pointer")(list);
+    };
 }
